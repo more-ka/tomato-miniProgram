3503cb19300830374834234b64e253fbc5a203ee
more-ka <1640627628@qq.com>
2019-10-28 22:39:28 +0800

    tabBar/button组件/clock组件

---
+++ components/g-button/g-button.js
@@ -0,0 +1,32 @@
+// components/g-button.js
+Component({
+
+  properties: {
+    text:{
+      type: String,
+      value: ""
+    },
+    size: {
+      type: String,
+      value: ""
+    },
+    change:{
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/g-clock/g-clock.js
@@ -0,0 +1,23 @@
+// components/g-button/g-clock.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})

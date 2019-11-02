eeba6ec901d19a6b317a4cd638c58e0f08ed1fa4
more-ka <1640627628@qq.com>
2019-11-02 11:58:29 +0800

    登录,保存token

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,36 @@
-const {app_id,app_secret} = getApp().globalData
-const {http} = require('../../utils/http.js')
+const { app_id, app_secret } = getApp().globalData;
+const { http } = require("../../utils/http.js");
 
 Page({
-  data: {
-
+  data: {},
+  onShow() {
+    http.get("/todos");
   },
-  onShow(){
-    http.get('/todos')
+  login(event) {
+    let encrypted_data = event.detail.encryptedData
+    let iv = event.detail.iv
+    let code
+    wx.login({
+      success: function(res) {
+        code = res.code
+        http.post('/sign_in/mini_program_user',{
+          code,
+          iv,
+          encrypted_data,
+          app_id,
+          app_secret
+        })
+        .then(response=>{
+          console.log(response);
+          
+          wx.setStorageSync('me', response.data.resource)
+          wx.setStorageSync('X-token', response.header["X-token"])
+          wx.reLaunch({
+            url: '/pages/index/index'
+          })
+        })
+      },
+      fail: function(res) {}
+    });
   }
-})
-
+});
--- utils/http.js
+++ utils/http.js
@@ -1,46 +1,44 @@
 let {host,t_app_id,t_app_secret} = getApp().globalData
 const _http = (method,url,data)=>{
-  return new Promise((resolve,reject)=>{
-    wx.request({
-      url: `${host}${url}`,
-      data,
-      header: {
-        Authorization: `Bearer ${wx.getStorageSync('X-token')}`,
-        "t-app-id": t_app_id,
-        "t-app-secret": t_app_secret
-      },
-      method,
-      dataType: 'json',
-      success: function(res) {
-        console.log(res);
-        
-        let statusCode = res.statusCode
-        if(statusCode>=400){
-          if(statusCode === 401) {
-            wx.redirectTo({
-              url: '/pages/login/login',
-            })
-          }
-          reject(statusCode,res)
-        }else{
-          resolve(statusCode,res)
-        }
-      },
-      fail: function(res) {
-        wx.showToast({title: '请求失败',icon: 'none'})
-        reject(res)
-      }
+    return new Promise((resolve,reject)=>{
+        wx.request({
+            url: `${host}${url}`,
+            data,
+            header: {
+                Authorization: `Bearer ${wx.getStorageSync('X-token')}`,
+                "t-app-id": t_app_id,
+                "t-app-secret": t_app_secret
+            },
+            method,
+            dataType: 'json',
+            success: function(res) {
+                let statusCode = res.statusCode
+                if(statusCode>=400){
+                    if(statusCode === 401) {
+                        wx.redirectTo({
+                            url: '/pages/login/login',
+                        })
+                    }
+                    reject(statusCode,res)
+                }else{
+                    resolve(res)
+                }
+            },
+            fail: function(error) {
+                wx.showToast({title: '请求失败',icon: 'none'})
+                reject(error)
+            }
+        })
     })
-  })
 }
 
 const http = {
-  get: (url,params)=> _http('GET',url,params),
-  post: (url,data)=>_http('POST',url,data),
-  put:(url,data)=>_http('PUT',url,data),
-  delete:(url,data)=>_http('DELETE',url,data)
+    get: (url,params)=> _http('GET',url,params),
+    post: (url,data)=>_http('POST',url,data),
+    put:(url,data)=>_http('PUT',url,data),
+    delete:(url,data)=>_http('DELETE',url,data)
 }
 
 module.exports = {
-  http
+    http
 }
